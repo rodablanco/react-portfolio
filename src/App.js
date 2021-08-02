3d4e13ac738afe52719e49166d709dad27c95371
Rod Blanco <rodrigo.a.blanco1990@gmail.com>
2021-08-01 18:15:24 -0700

    installed package.json and finish navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>HELLO MY NAME IS ROD THIS IS MY REACT PORTFOLIO</h1>
-    </div>
+  <div className="App">
+  
+     <Navbar/>
+  </div>
+
   );
 }
 
