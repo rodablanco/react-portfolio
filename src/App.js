74d5b062d046448ec4f736a893fa420471ff18ab
Rod Blanco <rodrigo.a.blanco1990@gmail.com>
2021-08-07 17:23:53 -0700

    major code addition from validation to icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AboutMe from "./components/AboutMe";
+import * as ReactBootStrap from "react-bootstrap";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import NotFound from "./pages/NotFound";
+import Resume from "./components/Resume"
+import GitHub from "./components/SocialMedia";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-  <div className="App">
-  
-     <Navbar/>
-  </div>
-
+    <div className="App">
+      <ReactBootStrap.Container fluid>
+        <Router>
+          <Navbar />
+      
+            <Switch>
+              <Route exact path="/">
+                
+                <AboutMe />
+              </Route>
+              <Route exact path="/about">
+              </Route>
+              {/* need to move */}
+              <Route exact path="/projects" component={Projects} />
+              <Route exact path="/contact">
+                <Contact />
+              </Route>
+              <Route exact path="/resume">
+                <Resume />
+              </Route>
+              <Route path="*" component={NotFound} />
+            </Switch>
+         
+        </Router>
+        <GitHub/>
+      </ReactBootStrap.Container>
+      
+    </div>
   );
 }
 
